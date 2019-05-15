d2a0c98fc35c74428158cdbfc748fadebb828a50
DamolaAdewunmi <damolasd@gmail.com>
2019-05-15 15:17:28 +0100

    Article exercise done, articles now expand at the click of the div

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -7,9 +7,10 @@ const toggleMenu = () => {
 // Start Here: Create a reference to the ".menu" class
 const menu;
 menu = document.querySelector('.menu');
+menu.display.style.color = 'red';
 // create a reference to the ".menu-button" class
 const menuButton;
 menuButton = document.querySelector('.menu-button');
 // Using your menuButton reference, add a click handler that calls toggleMenu
 
-menuButton.addEventListener('click', toggleMenu);
+menuButton.addEventListener('click', () => toggleMenu);
